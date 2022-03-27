72854d96d98264cf227faa07b59582e844d8a24e
RosaSerrano01 <rosacalixs0229@gmail.com>
2022-03-26 21:42:02 -1200

    Se agregan archivos del proyecto

---
+++ app/Http/Controllers/CantanteController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cantante;
+
+class CantanteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $data['cantantes']=Cantante::paginate(12);
+        return view('cantantes.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('cantantes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $cantanteData= request()->except('_token');
+        Cantante::insert($cantanteData);
+
+        //
+        return redirect('cantante');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $cantante=Cantante::findOrFail($id);
+        return view ('cantantes.edit', compact('cantante'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $cantanteData=request()->except(['_token', '_method']);
+        Cantante::where('id', '=', $id)->update($cantanteData);
+        return redirect('cantante');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        Cantante::destroy($id);
+        return redirect("cantante");
+    }
+}
---
+++ resources/views/cantantes/index.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <title>Listado de cantantes.</title>
+</head>
+<body class="bg-success p-2" style="--bs-bg-opacity: .4;">
+
+    <div id="body" class="bg-dark p-2" style="--bs-bg-opacity: .2;">
+<div class="alert alert-success text-center" role="alert">
+    <h1>Listado de cantantes.</h1>
+</div>
+<br>
+<a class="btn btn-success"  href="{{ url('/cantante/create') }}" role="button"> + Registrar un nuevo cantante.</a>
+<br>
+<br>
+<table class="table table-success table-hover table-bordered border-dark text-center">
+    <thread>
+        <tr>
+            <th>#</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Nombre artístico</th> 
+            <th>Edad</th>
+            <th>Género de música</th>
+            <th colspan="2" >Acciones</th>
+</tr>
+    </thread>
+    <tbody>
+        @foreach ($cantantes as $cantante)
+        <<tr>
+            <td>{{ $cantante->id }}</td>
+            <td>{{ $cantante->name }}</td>
+            <td>{{ $cantante->lastName }}</td>
+            <td>{{ $cantante->artisticName }}</td>
+            <td>{{ $cantante->age }}</td>
+            <td>{{ $cantante->musicGenre }}</td>
+
+            <td>
+                <a class="btn btn-outline-primary" href="{{ url('/cantante/' .$cantante->id. '/edit') }}" role="button">Editar</a>
+</td>
+<td>
+    <form action="{{ url('/cantante/' .$cantante->id) }}" method="POST">
+        @csrf 
+        {{ method_field('DELETE') }}
+        <input type="submit" class="btn btn-outline-danger" value="Eliminar"
+         onclick="return confirm('¿Realmente desea eliminar el registro?')">
+</form>
+</td>
+</tr>
+        @endforeach
+</tbody>
+</table>
+</div>
+
+<style>
+#body
+{
+margin-left:2%;
+margin-right:2%;
+margin-top:2%;
+margin-bottom:2%;
+border-radius:0.5em;
+}
+</style>
